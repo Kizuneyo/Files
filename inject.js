992be93952ce2319a62d2ff11c3cf32f54c6978a
Kizuneyo <92352339+Kizuneyo@users.noreply.github.com>
2021-10-11 21:28:37 -0700

    Update inject.js

--- inject.js
+++ inject.js
@@ -6,7 +6,7 @@ function inject()
     setTimeout(() => //Wait for AWS script to have initalised, it shouldnt take more than 5s
     {
         let tts = document.createElement("script");
-        tts.src = "http://wikisend.com/download/128798/tts.js";
+        tts.src = "https://cdn.jsdelivr.net/gh/Kizuneyo/Files@main/tts.js";
         document.body.appendChild(tts);
     }, 5000);
 }
