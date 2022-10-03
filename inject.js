ad27b0e101031d6862ac6119fbc89c1809530df0
Kizuneyo <92352339+Kizuneyo@users.noreply.github.com>
2022-10-03 08:28:50 -0700

    Update inject.js

--- inject.js
+++ inject.js
@@ -3,10 +3,10 @@ function inject()
     let aws = document.createElement("script");
     aws.src = "https://cdn.jsdelivr.net/gh/Kizuneyo/Files@main/aws-sdk.min.js";
     document.body.appendChild(aws);
-    setTimeout(() => //Wait for AWS script to have initalised, it shouldnt take more than 5s
+    setTimeout(() => //Wait for AWS script to have initalised
     {
         let tts = document.createElement("script");
-        tts.src = "https://cdn.jsdelivr.net/gh/Kizuneyo/Files@main/tts.js";
+        tts.src = "https://cdn.jsdelivr.net/gh/Kizuneyo/Files@main/inject.js";
         document.body.appendChild(tts);
     }, 5000);
 }
